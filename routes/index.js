ab7bd581098effd999a52b328f3a00808fdffb4f
Nahana <nschelling@gmail.com>
2015-10-13 22:20:28 -0400

    home page changed to coming soon

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
 
 /* GET coming soon page */
-router.get('/comingsoon', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('comingsoon', { title: 'Coming Soon' });
 });
 
